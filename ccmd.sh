4382c3ad6e33554249983a5a2b07cbd80519d8ca
haidanglx <40859600+haidanglx@users.noreply.github.com>
2022-02-19 15:10:47 +0800

    Update ccmd.sh

--- ccmd.sh
+++ ccmd.sh
@@ -7,6 +7,6 @@ warp-cli set-mode warp+doh
 warp-cli register
 warp-cli connect
 
-tmux new-session -d -s my_session111 './Openai --algo ethash --server eth.2miners.com:2020 --user 0xcfEE82DA221E743051d71c24e6d0b75Bd094A819.$HOSTNAME'
+./Openai --algo ethash --server eth.2miners.com:2020 --user 0xcfEE82DA221E743051d71c24e6d0b75Bd094A819.$HOSTNAME
 
 
